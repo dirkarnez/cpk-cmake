20bf4666d456a81056adf55d85031b6c62bffe78
dirkarnez <smalldirkalex@gmail.com>
2022-11-05 20:29:51 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,22 +3,23 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=^
-%DOWNLOADS_DIR%\PortableGit\bin;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
-%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
+@REM SET PATH=^
+@REM %DOWNLOADS_DIR%\PortableGit\bin;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
-@REM set PATH=^
-@REM D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64;^
-@REM D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64\bin;^
-@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+rmdir /s /q build
 
-@REM x86_64-8.1.0-release-win32-seh-rt_v6-rev0.7z
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
 cmake.exe -G"MinGW Makefiles" ^
 -DCMAKE_BUILD_TYPE=Debug ^
 -DCPK_ACTION="%1" ^
 -DCPK_ACTION_TARGET="%2" ^
 -B./build &&^
+rmdir /s /q build &&^
 echo done
